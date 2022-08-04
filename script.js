59b4c25516a31da01822909ae5dfb3c5dfe0f8fb
Gabriel <gabrielssquaresma@gmail.com>
2022-08-03 22:37:37 -0300

    condicionais

---
+++ script.js
@@ -0,0 +1,43 @@
+const bool1 = false
+const bool2 = true
+const bool3 = true
+
+/*if (bool1) {
+    alert('Entrei no if')
+}else{
+    alert('entrei no else')
+}*/
+
+//att boolean
+
+/*if (bool1 === bool2) {
+    alert('a 1 condição é verdadeira')
+}else if (bool2 === bool3) {
+    alert('a 2 condição é verdadeira')
+}else if (bool1 === bool3) {
+    alert('a 3 condição é verdadeira')
+}else {
+    alert('nenhuma é usável')
+};*/
+
+/*const idade = 28
+
+if (idade >= 18) {
+    alert("Deve emitir o título")
+}else {
+    alert('não pode emitir')
+}*/
+
+//Parte 1:
+
+const media = 'pedro';
+
+if (media >= 5){
+    alert('Tá salvo, campeão!')
+}else if (media >= 3){
+    alert('Recuperação, fi.')
+}else if (media <= 3){
+    alert('Vergonha da profission...')
+}else {
+    alert('Nada válido :C')
+};
